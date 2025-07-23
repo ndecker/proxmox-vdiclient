2ef7d2f0f2db1fba474b9d09eafdf2e3bb88083c
Nils Decker <nils@ndecker.de>
2025-07-23 10:37:43 +0200

    look for config file in current directory

--- configfile_flag.go
+++ configfile_flag.go
@@ -42,19 +42,10 @@ func (c *ConfigFileFlag) Set(fn string) error {
 // ReadDefault reads the default configuration file from UserConfigDir. Missing default config file is silently ignored.
 // Must be called before adding ConfigFileFlag to FlagSet because it sets the default value.
 func (c *ConfigFileFlag) ReadDefault(program string) error {
-	path, err := os.UserConfigDir()
-	if err != nil {
+
+	fn, f := c.findDefault(program)
+	if f == nil {
 		return nil
-	}
-
-	fn := filepath.Join(path, program, program+".conf")
-
-	f, err := os.Open(fn)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil // ignore missing default config file
-		}
-		return err
 	}
 	defer f.Close()
 
@@ -62,13 +53,37 @@ func (c *ConfigFileFlag) ReadDefault(program string) error {
 		c.LogPrintf("loading configuration file: %s", fn)
 	}
 
-	err = c.readConfig(f)
+	err := c.readConfig(f)
 	if err != nil {
 		return err
 	}
 
 	c.value = fn
 	return nil
+}
+
+func (c *ConfigFileFlag) findDefault(program string) (string, io.ReadCloser) {
+	var files []string
+
+	path, err := os.UserConfigDir()
+	if err == nil {
+		fn := filepath.Join(path, program, program+".conf")
+		files = append(files, fn)
+	}
+
+	files = append(files, filepath.Join(program+".conf"))
+
+	for _, fn := range files {
+		f, err := os.Open(fn)
+		if err == nil {
+			return fn, f
+		}
+		if !os.IsNotExist(err) {
+			c.LogPrintf("error loading config file: %s: %s", fn, err)
+		}
+		_ = f.Close()
+	}
+	return "", nil
 }
 
 func (c *ConfigFileFlag) readConfig(f io.Reader) error {
